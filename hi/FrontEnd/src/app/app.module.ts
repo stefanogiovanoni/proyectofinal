cde1e214600439198bbe9928ff355a28190ece2e
stefanogiovannoni <stefanogiovannonii@gmail.com>
2022-08-13 20:20:13 -0300

    carpeta frontend hecha

---
+++ hi/FrontEnd/src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoapComponent } from './components/logoap/logoap.component';
+import { SocialComponent } from './components/social/social.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AcerceComponent } from './components/acerce/acerce.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { HysComponent } from './components/hys/hys.component';
+import { ProyectoComponent } from './components/proyecto/proyecto.component';
+import { FooterComponent } from './components/footer/footer.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    LogoapComponent,
+    SocialComponent,
+    BannerComponent,
+    AcerceComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    HysComponent,
+    ProyectoComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    NgCircleProgressModule.forRoot({})
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ hi/FrontEnd/src/app/components/acerce/acerce.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-acerce',
+  templateUrl: './acerce.component.html',
+  styleUrls: ['./acerce.component.css']
+})
+export class AcerceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
